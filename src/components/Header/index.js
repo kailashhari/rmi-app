cfa65ca0cab9d76d798cd239db8a61eb0d37629d
Muhesh7 <vmspride7@gmail.com>
2022-11-16 23:54:20 +0530

    fix: header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,14 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Divider from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  styled,
-  Zoom,
-} from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import { styled, Zoom } from "@mui/material";
 import { colors, fontStyles } from "../../constants";
 import logo from "../../assets/logoCompressed.png";
 import { Link, useLocation } from "react-router-dom";
@@ -54,9 +47,9 @@ const NavItem = styled("div")({
   alignItems: "center",
 });
 
-const Indicator = ({ active }) => {
+const Indicator = () => {
   return (
-    <Zoom in={active}>
+    <Zoom in={true}>
       <div
         style={{
           width: "20px",
@@ -69,9 +62,16 @@ const Indicator = ({ active }) => {
 };
 
 function ResponsiveAppBar() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   const location = useLocation();
   let logoAnim;
   if (
@@ -84,260 +84,184 @@ function ResponsiveAppBar() {
   } else if (location.state.prevPath !== "/" && location.pathname === "/") {
     logoAnim = "zoomIn 0.4s reverse";
   }
-  const drawer = (
-    <Box
-      onClick={handleDrawerToggle}
+  // (location.state?.prevPath === '/' && location.pathname !== '/') ? 'zoomIn 0.4s forwards' : 'none';
+  console.log(location.state?.prevPath, location.pathname);
+
+  return (
+    <AppBar
+      position="fixed"
       sx={{
-        zIndex: 100,
-        textAlign: "center",
+        background: `linear-gradient(0deg, ${colors.dark}00 0%, ${colors.dark}FF 30%)`,
+        height: "6rem",
+        zIndex: 1000,
+        boxShadow: "none",
       }}
     >
-      <Box
+      <Container
+        maxWidth="xl"
         sx={{
-          display: "inline-flex",
-          flexDirection: "row",
-          marginLeft: "-5rem",
-          marginTop: "1.2rem",
-          marginBottom: "1.2rem",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <img
-          src={logo}
-          alt="logo"
-          style={{
-            display: "block",
-            margin: location.pathname === "/" ? "0" : "3rem",
-            padding: 0,
-            width: location.pathname === "/" ? "0" : "56px",
-            height: location.pathname === "/" ? "0" : "56px",
-            animation: logoAnim,
-          }}
-        />
-        <Typography
+        <Toolbar
           sx={{
-            ...fontStyles.title,
-            display: { md: "none" },
-            marginTop: "auto",
-            marginLeft: "-2.5rem",
+            height: "fit-content",
           }}
         >
-          RMI
-        </Typography>
-      </Box>
-      <Divider />
-      <List>
-        {pages.map((item) => (
-          <Link
-            key={item.text}
-            to={`/${item.route}`}
-            state={{ prevPath: location.pathname }}
+          <img
+            src={logo}
+            alt="logo"
             style={{
-              color: "inherit",
-              textDecoration: "none",
-              fontSize: "1.1rem",
+              display: "block",
+              margin: location.pathname === "/" ? "0" : "3rem",
+              padding: 0,
+              width: location.pathname === "/" ? "0" : "56px",
+              height: location.pathname === "/" ? "0" : "56px",
+              animation: logoAnim,
+            }}
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.text} onClick={handleCloseNavMenu}>
+                  <Typography
+                    textAlign="center"
+                    sx={{
+                      ...fontStyles.navlinks,
+                    }}
+                  >
+                    {page.text}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              position: "relative",
+              gap: "1rem",
             }}
           >
-            <ListItem disablePadding>
-              <ListItemButton
-                sx={{
-                  textAlign: "center",
-                  flexDirection: "column",
+            {/* <NavItem
+              key={'Home'}
+              style={{
+                ...fontStyles.navlinks,
+                color: location.pathname === '/' ? colors.primary : colors.light,
+              }}   
+            >
+              <Link to={`/`} state={{prevPath: location.pathname}} style={{
+                color: 'inherit',
+                textDecoration: 'none'
+              }}>Home</Link>
+              {location.pathname === '/' && <Indicator active={location.pathname === '/'}/>}
+            </NavItem>          */}
+
+            {pages.map((page) => (
+              <NavItem
+                key={page.text}
+                style={{
                   ...fontStyles.navlinks,
                   color:
-                    location.pathname === `/${item.route}` ||
-                    (item.route === "projects" &&
+                    location.pathname === `/${page.route}` ||
+                    (page.route === "projects" &&
                       location.pathname.length > 8 &&
                       location.pathname.slice(0, 8) === "/project")
                       ? colors.primary
                       : colors.light,
                 }}
               >
-                <ListItemText primary={item.text} />
-                <Indicator active={location.pathname === `/${item.route}`} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    </Box>
-  );
-
-  // location.state?.prevPath === "/" && location.pathname !== "/"
-  //   ? "zoomIn 0.4s forwards"
-  //   : "none";
-  console.log(location.state?.prevPath, location.pathname);
-
-  return (
-    <>
-      <AppBar
-        position="fixed"
-        sx={{
-          background: `linear-gradient(0deg, ${colors.dark}00 0%, ${colors.dark}FF 20%)`,
-          height: "6rem",
-          zIndex: 200,
-          boxShadow: "none",
-        }}
-      >
-        <Container
-          maxWidth="xl"
-          sx={{
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Toolbar
-            sx={{
-              height: "fit-content",
-              width: { xs: "100%", sm: "100%", md: "fit-content" },
-              justifyContent: "space-between",
-            }}
-          >
-            <Box sx={{ display: "flex", flexDirection: "row" }}>
-              <img
-                src={logo}
-                alt="logo"
-                style={{
-                  display: "block",
-                  margin: location.pathname === "/" ? "0" : "1rem",
-                  padding: 0,
-                  width: location.pathname === "/" ? "0" : "56px",
-                  height: location.pathname === "/" ? "0" : "56px",
-                }}
-              />
-              <Typography
-                sx={{
-                  ...fontStyles.title,
-                  display: {
-                    xs: location.pathname === "/" ? "none" : "block",
-                    md: "none",
-                  },
-                  margin: "auto",
-                  marginLeft: "-0.5rem",
-                }}
-              >
-                RMI
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: { xs: "flex", md: "none" },
-              }}
-            >
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                sx={{ mr: "0.1rem", display: { md: "none" } }}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Box>
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: { xs: "none", md: "flex" },
-                position: "relative",
-                gap: "1rem",
-              }}
-            >
-              {pages.map((page) => (
-                <NavItem
-                  key={page.text}
+                <Link
+                  to={`/${page.route}`}
+                  state={{ prevPath: location.pathname }}
                   style={{
-                    ...fontStyles.navlinks,
-                    color:
-                      location.pathname === `/${page.route}` ||
-                      (page.route === "projects" &&
-                        location.pathname.length > 8 &&
-                        location.pathname.slice(0, 8) === "/project")
-                        ? colors.primary
-                        : colors.light,
+                    color: "inherit",
+                    textDecoration: "none",
+                    fontSize: "1.1rem",
                   }}
                 >
-                  <Link
-                    to={`/${page.route}`}
-                    state={{ prevPath: location.pathname }}
-                    style={{
-                      color: "inherit",
-                      textDecoration: "none",
-                      fontSize: "1.1rem",
-                    }}
-                  >
-                    {page.text}
-                  </Link>
-                  {(location.pathname === `/${page.route}` ||
-                    (page.route === "projects" &&
-                      location.pathname.length > 8 &&
-                      location.pathname.slice(0, 8) === "/project")) && (
-                    <Indicator
-                      active={location.pathname === `/${page.route}`}
-                    />
-                  )}
-                </NavItem>
-              ))}
+                  {page.text}
+                </Link>
+                {(location.pathname === `/${page.route}` ||
+                  (page.route === "projects" &&
+                    location.pathname.length > 8 &&
+                    location.pathname.slice(0, 8) === "/project")) && (
+                  <Indicator active={location.pathname === `/${page.route}`} />
+                )}
+              </NavItem>
+            ))}
 
-              <a
-                href="https://www.youtube.com/c/RMIRoboticsandMachineIntelligenceNITT"
+            <a
+              href="https://www.youtube.com/c/RMIRoboticsandMachineIntelligenceNITT"
+              style={{
+                height: "36px",
+                marginBlock: "auto",
+                cursor: "pointer",
+              }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <YoutubeSvg
                 style={{
-                  height: "36px",
-                  marginBlock: "auto",
-                  cursor: "pointer",
+                  height: "34px",
                 }}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <YoutubeSvg
-                  style={{
-                    height: "34px",
-                  }}
-                />
-              </a>
-              <a
-                href="https://www.youtube.com/c/RMIRoboticsandMachineIntelligenceNITT"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/c/RMIRoboticsandMachineIntelligenceNITT"
+              style={{
+                height: "36px",
+                marginBlock: "auto",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubSvg
                 style={{
-                  height: "36px",
-                  marginBlock: "auto",
-                  cursor: "pointer",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  height: "28px",
+                  width: "36px",
                 }}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <GithubSvg
-                  style={{
-                    height: "28px",
-                    width: "36px",
-                  }}
-                />
-              </a>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Drawer
-        variant="persistent"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true, // Better open performance on mobile.
-        }}
-        sx={{
-          "& .MuiDrawer-paper": {
-            background: colors.dark,
-            boxSizing: "border-box",
-            width: 250,
-          },
-          display: { sm: "block", md: "none" },
-        }}
-      >
-        {drawer}
-      </Drawer>
-    </>
+              />
+            </a>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 export default ResponsiveAppBar;
